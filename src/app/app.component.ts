16c86db8c52fcfb6eefe675029450fbf06835cfd
Boateng Gideon <hack3rkofi@gmail.com>
2023-10-16 10:57:55 +0000

    complete Typing Game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typingGame';
+  randomSentence = faker.lorem.paragraph(4);
+  enteredText = '';
+  onKey(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomCharacter: string, enteredText: string) {
+    if (!enteredText) {
+      return 'pending';
+    }
+    return enteredText === randomCharacter ? 'correct' : 'incorrect';
+  }
+
+  refresh() {
+    this.randomSentence = faker.lorem.paragraph(4);
+    this.enteredText = '';
+  }
 }
